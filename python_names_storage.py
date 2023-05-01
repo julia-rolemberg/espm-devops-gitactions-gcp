b779a68fff2ade4a87cc4a0220416974741fe563
julia-rolemberg <61888090+julia-rolemberg@users.noreply.github.com>
2023-05-01 15:01:12 -0300

    add project

---
+++ python_names_storage.py
@@ -0,0 +1,44 @@
+from bs4 import BeautifulSoup
+from google.cloud import storage
+from google.oauth2 import service_account
+import requests
+from credentials import credential
+
+headers = {
+  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+}
+
+credentials_dict = credential
+
+try:
+
+  """Uploads a file to the bucket."""
+  credentials = service_account.Credentials.from_service_account_info(credentials_dict)
+  storage_client = storage.Client(credentials=credentials)
+  bucket = storage_client.get_bucket('bucket-gitactions-espm-2023') 
+  blob = bucket.blob('artist-names.csv')
+
+  pages = []
+  names = "Name \n"
+
+  for i in range(1, 5):
+    url = 'https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ' + str(i) + '.htm'
+    pages.append(url)
+
+  for item in pages:
+    page = requests.get(item)
+    soup = BeautifulSoup(page.text, 'html.parser')
+
+    last_links = soup.find(class_='AlphaNav')
+    last_links.decompose()
+
+    artist_name_list = soup.find(class_='BodyText')
+    artist_name_list_items = artist_name_list.find_all('a')
+
+    for artist_name in artist_name_list_items:
+      names = names + artist_name.contents[0] + "\n"
+
+    blob.upload_from_string(names, content_type="text/csv")
+
+except Exception as ex:
+  print(ex) 
