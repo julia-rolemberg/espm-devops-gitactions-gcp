170bf4ba57fdacc77fd106e31e62917cdde27cd5
julia-rolemberg <61888090+julia-rolemberg@users.noreply.github.com>
2023-05-01 15:03:24 -0300

    add credential

---
+++ credentials.py
@@ -0,0 +1,13 @@
+
+credential = {
+  "type": "service_account",
+  "project_id": "espm-dataops-2023",
+  "private_key_id": "e65d12040096e2ac46a67c97b50ad7730c7d25d9",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQDAsN+fCKxDDYUk\nRbGe4iR+0ElC0Q+J95HtkKKnpDOYCI6Jo3XrOZVN3mYAC+mbBtK30nyYy2m0IiE7\ntI0elSsKGkYRWvf6gzk0Azn9wWA92lLNfYja1DYUAyhIhlg7sVKuQm0WzySaIFgw\nisws/p+40eW85Lz79/1FslM2AfXYh5fxGtKDPcSaIn8xMZtWA84VVy7gJ3kvM0Kr\noQxw+YIEEWCdn+JMu74pF5f7K844SBXxRCRkgOgAcmdbegllk781sRhHCj19P9Rh\nxP9gAqfVzXA5sEFKJMXC+uxZgGcAXNAy6oqBgdzIU6+MUt2vAZMyV4vuC/cbHKYk\nK1jVBJnZAgMBAAECggEADy+hYBpB4UL2hEZN2lS+RZRKwc5SKDBvusZUW1vwiQHb\nofNU/sXaA3oMleJaddJS1xPqp+wYF489CUSQfqGI89HgRcvikUa4QmYisuIDoLxN\nHE7Rc1WYMXmSOi0EVSgDeSJZEJwgqVj1OlYmA0HJ+rnoHjv0vPLtbgnAzxfWdUsP\nZbsusjrTYEKdwzgyGLn8cUj16Q3FYVppUGXWeMp6+JIlfGRUbAevzpC30mxHjkJd\nKCNeoTvV4H/1A5dxMS7VeGcRAxap6CwVENgOAWlUfWyXMK+ckxmFmD09TecEbgbQ\nUTMJ6uv77LwLZUOokP3IioMDX957I7nsQEiRSW9h1QKBgQDiYHQ+jPVEPX+277ov\n+onqLPBdPShDhpgYdKwxV1HAD+JOAaky23vp//T7xaDn5FsHnrAuFYVZ9hLBf1Mx\nlVzjoAu+I2D0wHH92PeScklGap9yJL5lCvH2QBpH9ilIHcfzTuzwyVV/isu4hMQ0\nif5DZbssZ8dF1qA82ThpZquQywKBgQDZ5/RSw7OhK1HXrarmrWbmE0E9SjF+9Urj\nGWiYWvvH8tb+dZwj800FXZkMw0qqVAd0APgLSz4D41M7OETwdyVUQxg4xlj9dRW+\nf9NIFETCrRimPXLdDeCMgxWAGZKU2JtjaFwDygV2uvrl9s1E1/j51wqcJcYOF2Ea\nOjSHvgOfawKBgDd5Y4Mgj8oVOALgZEP1pW5ZbN2sav2BMixsCvZca5AhVFfBWHE6\nD/eb3bEFERVChHwtiwcABxmwj5ATX5O7AnRVeAs0KEJSFp0aWjDtCL/UyRXzoySn\ntfueYdJYpG0jvygEo1OuQuFqZdnlzx8A4eoqN9/93ntiyQmgQY6ytkBZAoGAdHt6\n7pcwubHngQyT89Wz0L9wO2wiw0QqcPWPJQpV10+nv8Rs4AbaIEklXF401pIuncM6\n1Qa2sutc/aYZrBV0vfNTyl21CmABbKD192zmSM6xA4SLl0EozNPxtVV2Nt0HS39o\n2JmwjARtxGpG1ktBC4T5ZfkHQ1caECQXGozncNsCgYBapAtT1LT6hjgjo38euoNy\nB+shWT3KhH+RgzbgIPNtLgzv5y7Ms5XNiveLB5QCD9/7v0gVTGy2Hr6xFmttkmmz\nd6FloBB79+W1a7yHhh4XbHeECiydTx8/5haAW/YVphhr+zcp8Uo5ra5AQcDACGEd\nGX4V8U91hEOF5Zuyyv0PHg==\n-----END PRIVATE KEY-----\n",
+  "client_email": "sa-gitactions@espm-dataops-2023.iam.gserviceaccount.com",
+  "client_id": "113023218560852519141",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/sa-gitactions%40espm-dataops-2023.iam.gserviceaccount.com"
+}
